24c6658e13ccffdd5bb4ddecf182f11c06397520
Cynthia Hom <chom@g.hmc.edu>
2022-02-14 15:31:34 -0800

    Finished in class assignment!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+const data = [
+  {
+    id: 512,
+    name: "Neil Rhodes",
+    email: "rhodes@hmc.edu",
+    phone: "(909) 555-1212"
+  },
+  {
+    id: 787,
+    name: "Barack Obama",
+    email: "ex-prez@whitehouse.gov",
+    phone: "(312) 555-1212"
+  }
+];
+
+function PersonInfo(props) {
+  return <div className={"persondiv"}>
+    <span id={"name"}>{props.name}</span>
+    <span id={"email"}>{props.email}</span>
+    <span id={"phone"}>{props.phone}</span>
+  </div>
+}
+
+function ListofPeople(props) {
+  return props.people.map(person =>
+      <PersonInfo key={person.id} {...person} />);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ListofPeople people={data}/>
   );
 }
 
